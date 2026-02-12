668941ecee0a6cbe4ba8907e06bddc5b4d1ea83e
Rony Antolinez <rony1710.dev@gmail.com>
2026-02-12 18:01:23 -0500

    wip: add utils to get and format pokemon id

---
+++ src/lib/formatPokemonId.ts
@@ -0,0 +1,11 @@
+export function formatPokemonId(id: number): string {
+  if (id < 10) {
+    return `#00${id}`;
+  }
+
+  if (id >= 10 && id < 100) {
+    return `#0${id}`;
+  }
+
+  return `#${id}`;
+}
