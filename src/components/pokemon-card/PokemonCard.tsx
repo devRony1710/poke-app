2ec99fe681709b8f5b460b110d7103943b4c717c
Rony Antolinez <rony1710.dev@gmail.com>
2026-02-12 18:44:01 -0500

    wip: add test for pokemon card

--- src/components/pokemon-card/PokemonCard.tsx
+++ src/components/pokemon-card/PokemonCard.tsx
@@ -9,14 +9,27 @@ export const PokemonCard: FC<PokemonCardProps> = ({
   onClickCard,
 }) => {
   return (
-    <div className="pokemon-card-container">
-      <button className="pokemon-info-wrapper" onClick={onClickCard}>
-        <span className="pokemon-number">{pokemonId}</span>
-        <img src={pokemonImg} alt="pokemon image" className="pokemon-image" />
-        <span className="pokemon-name">{pokemonName}</span>
+    <div data-testid="pokemon-card-container" className="pokemon-card-container">
+      <button
+        data-testid="pokemon-card-button"
+        className="pokemon-info-wrapper"
+        onClick={onClickCard}
+      >
+        <span data-testid="pokemon-card-id" className="pokemon-number">
+          {pokemonId}
+        </span>
+        <img
+          data-testid="pokemon-card-image"
+          src={pokemonImg}
+          alt="pokemon image"
+          className="pokemon-image"
+        />
+        <span data-testid="pokemon-card-name" className="pokemon-name">
+          {pokemonName}
+        </span>
       </button>
 
-      <div className="background-gray" />
+      <div className="background-gray" data-testid="pokemon-card-bg" />
     </div>
   );
 };
