a89b3cbea676641a623c4ea165713207591d8f90
Rony Antolinez <rony1710.dev@gmail.com>
2026-02-12 11:32:21 -0500

    wip: add list of pokemon section template

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,13 @@
 import { Header } from '@/components/header/Header';
 import './Home.css';
+import { ListOfPokemonsTemplate } from '@/templates/list-of-pokemons-template/ListOfPokemonsTemplate';
 
 export const HomePage = () => {
   return (
     <section className="home-container">
       <Header />
+
+      <ListOfPokemonsTemplate />
     </section>
   );
 };
---
+++ src/templates/list-of-pokemons-template/ListOfPokemonsTemplate.tsx
@@ -0,0 +1,9 @@
+import './ListOfPokemons.css';
+
+export const ListOfPokemonsTemplate = () => {
+  return (
+    <section className="list-pokemons-main-wrapper">
+      <div>hola mundo</div>
+    </section>
+  );
+};
