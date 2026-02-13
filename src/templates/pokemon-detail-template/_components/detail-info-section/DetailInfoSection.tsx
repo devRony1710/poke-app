7f94f69db43e8e8e21592b7a967d0eda573ebec0
Rony Antolinez <rony1710.dev@gmail.com>
2026-02-12 21:59:00 -0500

    wip: add detail info section with pokemon with arrows component

---
+++ src/templates/pokemon-detail-template/_components/detail-info-section/DetailInfoSection.tsx
@@ -0,0 +1,10 @@
+import './DetailInfoSection.css';
+import { PokemonImageWithButtons } from '../pokemon-image-with-buttons/PokemonImageWithButtons';
+
+export const DetailInfoSection = () => {
+  return (
+    <section className="detail-info-wrapper">
+      <PokemonImageWithButtons />
+    </section>
+  );
+};
