07f4a1d0bfefbcbc0d4931a7f15bf744cb1bf2b9
Rony Antolinez <rony1710.dev@gmail.com>
2026-02-13 01:02:07 -0500

    wip: change chevron disable state styles

--- src/templates/pokemon-detail-template/_components/pokemon-image-with-buttons/PokemonImageWithButtons.tsx
+++ src/templates/pokemon-detail-template/_components/pokemon-image-with-buttons/PokemonImageWithButtons.tsx
@@ -5,19 +5,32 @@ import { useAppContext } from '@/hooks/use-intersection-observer/use-app-context
 
 export const PokemonImageWithButtons = () => {
   const { selectedPokemon, setSelectedPokemon } = useAppContext();
+
+  const handleChevronLeft = () => {
+    if (selectedPokemon === 1) {
+      return null;
+    }
+    setSelectedPokemon(selectedPokemon ? selectedPokemon - 1 : null);
+  };
+
+  const handleChevronRight = () => {
+    setSelectedPokemon(selectedPokemon ? selectedPokemon + 1 : null);
+  };
+
   return (
     <div className="detail-pokemon-image-wrapper">
-      <button className="chevron-button" type="button">
+      <button
+        className="chevron-button"
+        type="button"
+        onClick={handleChevronLeft}
+        disabled={selectedPokemon === 1}
+      >
         <ChevronLeft className="chevron-styles" />
       </button>
 
       <img className="pokemon-image-with-button" src={PikachuImg} alt="pikachu" />
 
-      <button
-        className="chevron-button"
-        type="button"
-        onClick={() => setSelectedPokemon(selectedPokemon ? selectedPokemon + 1 : null)}
-      >
+      <button className="chevron-button" type="button" onClick={handleChevronRight}>
         <ChevronRight className="chevron-styles" />
       </button>
     </div>
